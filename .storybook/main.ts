7d18160ccf3fe1ae3f9432bda826b0c35365099d
RonaldChicaVera <rchica@rootstack.com>
2022-12-03 18:27:03 -0500

    storybook config

---
+++ .storybook/main.ts
@@ -0,0 +1,15 @@
+module.exports = {
+  stories: ['../src/**/*.stories.(ts|tsx|js|jsx)'],
+  addons: [
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+    '@storybook/addon-knobs',
+    '@storybook/preset-create-react-app',
+    {
+      name: '@storybook/addon-docs',
+      options: {
+        configureJSX: true,
+      },
+    },
+  ],
+};
